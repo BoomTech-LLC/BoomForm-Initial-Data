ffc0b09407ef9d07ba3a0476aef584c70088c8e2
Tigran0199 <cruelman11@gmail.com>
2023-03-14 16:13:47 +0400

    feat: Added initials for fields and forms

---
+++ index.js
@@ -0,0 +1,4 @@
+import { fieldsInitials } from "./src/fields";
+import { formInitials } from "./src/form";
+
+export default { fieldsInitials, formInitials };
---
+++ src/form.js
@@ -0,0 +1,142 @@
+export const formInitials = {
+  current: null,
+  isLoading: false,
+  error: null,
+  fonts: [
+    {
+      family: "Quicksand",
+      variants: ["300", "regular", "500", "600", "700"],
+      subsets: ["latin", "latin-ext", "vietnamese"],
+      version: "v30",
+      files: {
+        300: "http://fonts.gstatic.com/s/quicksand/v30/6xK-dSZaM9iE8KbpRA_LJ3z8mH9BOJvgkKEo18G0wx40QDw.ttf",
+        500: "http://fonts.gstatic.com/s/quicksand/v30/6xK-dSZaM9iE8KbpRA_LJ3z8mH9BOJvgkM0o18G0wx40QDw.ttf",
+        600: "http://fonts.gstatic.com/s/quicksand/v30/6xK-dSZaM9iE8KbpRA_LJ3z8mH9BOJvgkCEv18G0wx40QDw.ttf",
+        700: "http://fonts.gstatic.com/s/quicksand/v30/6xK-dSZaM9iE8KbpRA_LJ3z8mH9BOJvgkBgv18G0wx40QDw.ttf",
+        regular:
+          "http://fonts.gstatic.com/s/quicksand/v30/6xK-dSZaM9iE8KbpRA_LJ3z8mH9BOJvgkP8o18G0wx40QDw.ttf",
+      },
+      category: "sans-serif",
+      kind: "webfonts#webfont",
+    },
+    {
+      family: "ABeeZee",
+      variants: ["regular", "italic"],
+      subsets: ["latin", "latin-ext"],
+      version: "v22",
+      files: {
+        regular:
+          "http://fonts.gstatic.com/s/abeezee/v22/esDR31xSG-6AGleN6tKukbcHCpE.ttf",
+        italic:
+          "http://fonts.gstatic.com/s/abeezee/v22/esDT31xSG-6AGleN2tCklZUCGpG-GQ.ttf",
+      },
+      category: "sans-serif",
+      kind: "webfonts#webfont",
+    },
+    {
+      family: "Abhaya Libre",
+      variants: ["regular", "500", "600", "700", "800"],
+      subsets: ["latin", "latin-ext", "sinhala"],
+      version: "v13",
+      files: {
+        500: "http://fonts.gstatic.com/s/abhayalibre/v13/e3t5euGtX-Co5MNzeAOqinEYj2ryqtxI6oYtBA.ttf",
+        600: "http://fonts.gstatic.com/s/abhayalibre/v13/e3t5euGtX-Co5MNzeAOqinEYo23yqtxI6oYtBA.ttf",
+        700: "http://fonts.gstatic.com/s/abhayalibre/v13/e3t5euGtX-Co5MNzeAOqinEYx2zyqtxI6oYtBA.ttf",
+        800: "http://fonts.gstatic.com/s/abhayalibre/v13/e3t5euGtX-Co5MNzeAOqinEY22_yqtxI6oYtBA.ttf",
+        regular:
+          "http://fonts.gstatic.com/s/abhayalibre/v13/e3tmeuGtX-Co5MNzeAOqinEge0PWovdU4w.ttf",
+      },
+      category: "serif",
+      kind: "webfonts#webfont",
+    },
+    {
+      family: "Bungee",
+      variants: ["regular"],
+      subsets: ["latin", "latin-ext", "vietnamese"],
+      version: "v11",
+      files: {
+        regular:
+          "http://fonts.gstatic.com/s/bungee/v11/N0bU2SZBIuF2PU_ECn50Kd_PmA.ttf",
+      },
+      category: "display",
+      kind: "webfonts#webfont",
+    },
+    {
+      family: "Abel",
+      variants: ["regular"],
+      subsets: ["latin"],
+      version: "v18",
+      files: {
+        regular:
+          "http://fonts.gstatic.com/s/abel/v18/MwQ5bhbm2POE6VhLPJp6qGI.ttf",
+      },
+      category: "sans-serif",
+      kind: "webfonts#webfont",
+    },
+  ],
+  styles: {
+    background: {
+      color: {
+        type: 0,
+        style: "#f8f8f8",
+        value: {
+          alpha: 1,
+          blue: 248,
+          green: 248,
+          hue: 0,
+          red: 248,
+          saturation: 0,
+          value: 97,
+        },
+      },
+    },
+    page: {
+      widthMeasurement: "Percent",
+      alignment: "left",
+      border: {
+        radius: 16,
+        size: 0,
+        color: {
+          isStyleVisible: true,
+          solid: "#434e5a33",
+          gradient: null,
+          imageUrl: null,
+          lastModified: {
+            style: "#434e5a33",
+            type: 0,
+            value: {
+              alpha: 0.2,
+              blue: 90,
+              green: 78,
+              hue: 211,
+              red: 67,
+              saturation: 26,
+              value: 35,
+            },
+          },
+        },
+      },
+      fill: {
+        color: {
+          isStyleVisible: true,
+          solid: "#fff",
+          gradient: null,
+          imageUrl: null,
+          lastModified: {
+            style: "#fff",
+            type: 0,
+            value: {
+              alpha: 1,
+              blue: 255,
+              green: 255,
+              hue: 0,
+              red: 255,
+              saturation: 0,
+              value: 100,
+            },
+          },
+        },
+      },
+    },
+  },
+};
