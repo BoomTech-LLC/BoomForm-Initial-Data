ed022de2a9881bc9ed40ba8300686bc9af3a1f2e
Tigran0199 <cruelman11@gmail.com>
2024-03-05 09:40:26 +0400

    feat: Added payment initials

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { fieldsInitials } from "./src/fields";
 import { formInitials } from "./src/form";
+import { paymentInitial } from "./src/payment";
 
 const initials = {
   fieldsInitials,
   formInitials,
+  paymentInitial,
 };
 
 export default initials;
---
+++ src/payment.js
@@ -0,0 +1,42 @@
+export const paymentInitial = {
+  paymentSettings: {
+    paymentID: "",
+    enablePromocode: true,
+    fee: 0,
+    paymentMethod: {
+      cash: false,
+      paypal: false,
+      stripe: false,
+    },
+    paymentReceipt: {
+      name: "",
+      email: "example@example.com",
+      phone: "",
+      address: "",
+      enabled: false,
+      template: "",
+    },
+    paymentType: 1,
+    shippingFee: 0,
+    showTotal: false,
+    redirectUrl: "",
+    promocodes: [],
+    payableFields: {
+      ONE_TIME: [],
+      SUBSCRIPTION: [],
+    },
+  },
+  promocode: {
+    _id: "",
+    applyTo: 2,
+    code: "",
+    discount: "",
+    discountType: 1,
+    field: [{ field: "", options: [""] }],
+    isActive: true,
+    limitCount: "",
+    limitDate: "",
+    limitType: 3,
+    payment: "",
+  },
+};
